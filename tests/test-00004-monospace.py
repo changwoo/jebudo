5cd8fe2e6a2493c3690ee571efacf17a48ddc36e
Changwoo Ryu <cwryu@debian.org>
2011-11-26 04:48:26 +0900

    Correct metadata for monospace

---
+++ tests/test-00004-monospace.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import fontforge
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: %s <sfd>" % sys.argv[0])
+    sys.exit(1)
+
+filename = sys.argv[1]
+basename = os.path.basename(filename)
+
+if not 'Mono' in basename:
+    sys.exit(0)
+
+font = fontforge.open(filename)
+
+assert(font.os2_panose[3] == 4)
+
+
